cf0d2c155b4c539c6033d628d9fef1bbc644679f
MorningSage <51978143+MorningSage@users.noreply.github.com>
2021-05-29 21:26:43 -0400

    general cleanup and update mappings

--- src/main/java/com/mekanism/Mekanism.java
+++ src/main/java/com/mekanism/Mekanism.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package com.mekanism;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class Mekanism implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
