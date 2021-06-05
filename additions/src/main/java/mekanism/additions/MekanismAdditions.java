1093181f29cf20f269ae43156fb39ca555ce00d5
MorningSage <51978143+MorningSage@users.noreply.github.com>
2021-06-05 02:34:29 -0400

    Forgot additions

---
+++ additions/src/main/java/mekanism/additions/MekanismAdditions.java
@@ -0,0 +1,12 @@
+package mekanism.additions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MekanismAdditions implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Additions Loaded");
+    }
+
+}
---
+++ additions/src/testmod/java/mekanism/additions/testmod/MekanismAdditionsTestMod.java
@@ -0,0 +1,12 @@
+package mekanism.additions.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MekanismAdditionsTestMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Additions Test Mod Initialized");
+    }
+
+}
