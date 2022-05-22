15ceb166cca7583fe163f5b5b17dd671b540dfe3
crashniels <nielswest@live.de>
2022-05-22 02:21:58 +0200

    Fix EntityType Attributes

--- mekanism/src/main/java/mekanism/registration/impl/EntityTypeDeferredRegister.java
+++ mekanism/src/main/java/mekanism/registration/impl/EntityTypeDeferredRegister.java
@@ -3,6 +3,7 @@ package mekanism.registration.impl;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
+
 import mekanism.Mekanism;
 import mekanism.registration.WrappedDeferredRegister;
 import mekanism.registration.impl.EntityTypeRegistryObject;
@@ -15,15 +16,13 @@ import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 
 public class EntityTypeDeferredRegister extends WrappedDeferredRegister<EntityType<?>> {
 
-    private Map<EntityTypeRegistryObject<? extends LivingEntity>, Supplier<AttributeSupplier.Builder>> livingEntityAttributes = new HashMap<>();
-
     public EntityTypeDeferredRegister(String modid) {
         super(modid, Registry.ENTITY_TYPE);
     }
 
     public <ENTITY extends LivingEntity> EntityTypeRegistryObject<ENTITY> register(String name, EntityType.Builder<ENTITY> builder, Supplier<AttributeSupplier.Builder> attributes) {
         EntityTypeRegistryObject<ENTITY> entityTypeRO = register(name, builder);
-        livingEntityAttributes.put(entityTypeRO, attributes);
+        FabricDefaultAttributeRegistry.register(entityTypeRO.getEntityType(), attributes.get());
         return entityTypeRO;
     }
 
@@ -31,17 +30,4 @@ public class EntityTypeDeferredRegister extends WrappedDeferredRegister<EntityTy
         return register(name, () -> builder.build(name), EntityTypeRegistryObject::new);
     }
 
-    private void registerEntityAttributes(EntityType<? extends LivingEntity> type, AttributeSupplier.Builder builder) {
-
-        if (livingEntityAttributes == null) {
-            Mekanism.logger.error("GlobalEntityTypeAttributes have already been set. This should not happen.");
-        } else {
-            //Register our living entity attributes
-            for (Map.Entry<EntityTypeRegistryObject<? extends LivingEntity>, Supplier<AttributeSupplier.Builder>> entry : livingEntityAttributes.entrySet()) {
-                FabricDefaultAttributeRegistry.register(type, builder);
-            }
-            //And set the map to null to allow it to be garbage collected
-            livingEntityAttributes = null;
-        }
-    }
 }
